6d4dd8f07427684f2111465b65db5c8fada2d262
Tylryan <tlrrn5@gmail.com>
2021-01-07 20:27:08 -0600

    Starting Sharpe Ratios

--- main.py
+++ main.py
@@ -105,3 +105,57 @@ print(f'The following portfolios are less risky than the SP500:\n\n'
 combined_annualized_std = combined_daily_std * np.sqrt(250)
 print(combined_annualized_std)
 
+# Rolling statistics
+
+## 21-day rolling standard deviation
+
+# rolling_std_21 = combined_return_df.rolling(window = 21).std()
+# rolling_std_21.plot(figsize = (20,10))
+# ylabel = plt.ylabel('Standard Deviation')
+# title = plt.title('21-Day Rolling Standard Deviation of All Portfolios')
+# plt.show()
+
+## Calculating and plotting the correlation of all the portfolios.
+# correlation = combined_return_df.corr()
+# sns.heatmap(correlation,vmax = -1, vmin = 1, annot = True)
+# plt.show()
+
+# Calculate and plot Beta for each portfolio.
+
+## Finding each portfolio
+soros_fund = whale_returns.iloc[:,0]
+paulson_fund = whale_returns.iloc[:,1]
+tiger_fund = whale_returns.iloc[:,2]
+berkshire_fund = whale_returns.iloc[:,3]
+
+
+## Finding the variance and covariance and beta.
+sp500_variance = sp500_returns['SP500 Returns'].var()
+
+algo1_beta = algo_returns['Algo 1'].cov(sp500_returns['SP500 Returns']) / sp500_variance
+algo2_beta = algo_returns['Algo 2'].cov(sp500_returns['SP500 Returns']) / sp500_variance
+soros_beta = soros_fund.cov(sp500_returns['SP500 Returns']) / sp500_variance
+paulson_beta = paulson_fund.cov(sp500_returns['SP500 Returns']) / sp500_variance
+tiger_beta = tiger_fund.cov(sp500_returns['SP500 Returns']) / sp500_variance
+berkshire_beta = berkshire_fund.cov(sp500_returns['SP500 Returns']) / sp500_variance
+
+## Finding the 21-day rolling average beta for soros_fund
+
+soros_rolling_covar  = soros_fund.rolling(window = 21).cov(sp500_returns['SP500 Returns'])
+rolling_var = sp500_returns['SP500 Returns'].rolling(window = 21).var()
+
+soros_rolling_beta = (soros_rolling_covar / rolling_var).dropna()
+
+# soros_rolling_beta.plot()
+# plt.show()
+
+# Rolling Beta with EWM
+# soros_rolling_covar  = soros_fund.ewm(halflife= '21 days', times = soros_fund.index).cov(sp500_returns['SP500 Returns'])
+# rolling_var = sp500_returns['SP500 Returns'].rolling(window = 21).var()
+
+
+#Exponentially Weighted Average of the returns of Soros's Fund.
+soros_ewm_returns= soros_fund.ewm(halflife='21 days', times = soros_fund.index).mean()
+#
+# a.plot()
+# plt.show()
