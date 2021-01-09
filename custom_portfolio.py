9504beef5e18c0db0c7c962021270dd49e1b7dff
Tylryan <tlrrn5@gmail.com>
2021-01-09 11:30:22 -0600

    1/9/21

--- custom_portfolio.py
+++ custom_portfolio.py
@@ -11,4 +11,46 @@ chgg_df = pd.read_csv('Custom_Portfolio/CHGG.csv', index_col='Date', parse_dates
 ea_df = pd.read_csv('Custom_Portfolio/EA.csv', index_col='Date', parse_dates=True, infer_datetime_format=True)
 spy_df = pd.read_csv('Custom_Portfolio/SPY.csv', index_col='Date', parse_dates=True, infer_datetime_format=True)
 
-print(spy_df)
+
+combined_df = pd.concat([axp_df, chgg_df,ea_df,spy_df], axis = 'columns', join = 'inner').sort_index()
+combined_df.columns = ['AXP','CHGG','EA','SPY']
+# print(combined_df.head())
+
+#! Calculating the daily returns of all portfolios
+combined_daily_returns = combined_df.pct_change().dropna()
+print(combined_daily_returns.head())
+
+#! Finding the weighted average assuming you invested equally in all three stocks
+
+weights = [0.25,0.25,0.25,0.25]
+print(weights)
+
+weighted_portfolio = combined_daily_returns.dot(weights)
+weighted_portfolio.columns = ['Portfolio Weighted Return']
+print(weighted_portfolio)
+
+combined_daily_returns['Portfolio Weighted Returns'] = weighted_portfolio
+nan_check = combined_daily_returns.isna().sum()
+print(combined_daily_returns.head())
+#* Calculate the annualized standard deviation of all the columns
+#! See if your your portfolio is more or less risky compared to the individual stocks.
+
+combined_df_standard_deviation = combined_daily_returns.std() * np.sqrt(252)
+print(combined_df_standard_deviation)
+
+riskier_stock = []
+less_risky_stock = []
+for i in combined_df_standard_deviation.index:
+    stock = i
+    std = combined_df_standard_deviation[i]
+    portfolio_std = combined_df_standard_deviation[4]
+    if std > portfolio_std:
+        riskier_stock.append([i,combined_df_standard_deviation[i]])
+    elif std < portfolio_std:
+        less_risky_stock.append([i,combined_df_standard_deviation[i]])
+
+print(f'The following stocks have more risk than the combined portfolio \n\n'
+      f'{riskier_stock} \n\n')
+
+print(f'The following stocks have less risk than the combined portfolio \n\n'
+      f'{less_risky_stock}')
